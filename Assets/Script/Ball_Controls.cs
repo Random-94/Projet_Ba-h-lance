19883fa21715c5f51ce74cf17f7f3dc08edaba99
Randoom <m.bachich@etudiant.ecolemelies.fr>
2020-12-30 20:17:02 +0100

    tir ok

--- Assets/Script/Ball_Controls.cs
+++ Assets/Script/Ball_Controls.cs
@@ -22,19 +22,34 @@ public class Ball_Controls : MonoBehaviour
     private bool IsMove; //savoir si la balle est à l’arrêt ou pas
     private bool IsPressed = false; //savoir si on a appuyer
     private Controls controls;
-    private Vector2 a; // le point "a" correspond a la premiere postion de ma souris chaque frame
+    private Vector2 mousePos; // le point "a" correspond a la premiere postion de ma souris chaque frame
     private Vector2 Resolution; //correspond a la variable qui va contenir la resolution de l'ecran
-    private Vector2 b;
-    //test
+    private Vector2 a, b;
+    
+    //question
     [SerializeField] private Transform ball;
     [SerializeField] private Transform RespawnPoint;
     [SerializeField] private Transform RespawnPoint2;
+    [SerializeField] private Transform RespawnPoint3;
+    [SerializeField] private Transform RespawnPoint4;
+    [SerializeField] private Transform RespawnPoint5;
+    [SerializeField] private Transform RespawnPoint6;
+    [SerializeField] private Transform RespawnPoint7;
+    [SerializeField] private Transform RespawnPoint8;
+    [SerializeField] private Transform RespawnPoint9;
+    [SerializeField] private Transform RespawnPoint10;
     [SerializeField] private TextMeshProUGUI Question1;
     [SerializeField] private TextMeshProUGUI Question2;
+    [SerializeField] private TextMeshProUGUI Question3;
+    [SerializeField] private TextMeshProUGUI Question4;
+    [SerializeField] private TextMeshProUGUI Question5;
+    [SerializeField] private TextMeshProUGUI Question6;
+    [SerializeField] private TextMeshProUGUI Question7;
+    [SerializeField] private TextMeshProUGUI Question8;
+    [SerializeField] private TextMeshProUGUI Question9;
+    [SerializeField] private TextMeshProUGUI Question10;
 
-
-    //[SerializeField] private Transform RespawnPoint3;
-    //[SerializeField] private Transform RespawnPoint4;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -44,7 +59,14 @@ public class Ball_Controls : MonoBehaviour
         myRB = GetComponent<Rigidbody>();
 
         Question2.GetComponent<TextMeshProUGUI>().enabled = false;
-
+        Question3.GetComponent<TextMeshProUGUI>().enabled = false;
+        Question4.GetComponent<TextMeshProUGUI>().enabled = false;
+        Question5.GetComponent<TextMeshProUGUI>().enabled = false;
+        Question6.GetComponent<TextMeshProUGUI>().enabled = false;
+        Question7.GetComponent<TextMeshProUGUI>().enabled = false;
+        Question8.GetComponent<TextMeshProUGUI>().enabled = false;
+        Question9.GetComponent<TextMeshProUGUI>().enabled = false;
+        Question10.GetComponent<TextMeshProUGUI>().enabled = false;
 
     }
 
@@ -75,10 +97,10 @@ public class Ball_Controls : MonoBehaviour
     private void OnAimPerformed(InputAction.CallbackContext obj)
     {
         
-        a = obj.ReadValue<Vector2>(); // permet de lire la valeur de "a" 
-        a /= Resolution;
-        a.x = Mathf.Clamp(a.x, 0, 1);
-        a.y = Mathf.Clamp(a.y, 0, 1);
+        mousePos = obj.ReadValue<Vector2>(); // permet de lire la valeur de "a" 
+        mousePos /= Resolution;
+        mousePos.x = Mathf.Clamp(mousePos.x, 0, 1);
+        mousePos.y = Mathf.Clamp(mousePos.y, 0, 1);
         //Debug.Log(a);
         b = obj.ReadValue<Vector2>(); // permet de lire la valeur de "a" 
         b /= Resolution;
@@ -96,12 +118,12 @@ public class Ball_Controls : MonoBehaviour
             Mycam.m_XAxis.m_MaxSpeed = 0;
             Mycam.m_YAxis.m_MaxSpeed = 0;
         }
-        
+        a = mousePos;
         
         //recuperer la position de la souris
         //vector faut les multiplier entre eux
 
-        Debug.Log(a);
+        Debug.Log(mousePos);
 
        
     }
@@ -111,22 +133,27 @@ public class Ball_Controls : MonoBehaviour
         //recuperer la position de la souris 
         //ensuite, creer le vecteur allant de la premiere position de la souris a celle recupere a la ligne precedente
         //"force" sera la magnitude du vecteur recuperé avant
-        Debug.Log(b);
 
-        if (!IsMove)
-        { 
+        b = mousePos;
+        /*
+         * var posAddForce = b - a;          
+           var posAdvancedLengh = posAddForce.magnitude; equivaut a "var magnitude = Vector2.Distance(a, b);"
+        */
+
+        var magnitude = Vector2.Distance(a, b);
+
         var camForward = Camera.main.transform.forward;
         var camDir = new Vector3(camForward.x, 0.0f, camForward.z);
-        myRB.AddForce(camDir * Force, ForceMode.Impulse);
+        myRB.AddForce(camDir * Force * magnitude, ForceMode.Impulse);
         IsMove = true;
-        }
-
+        
+        
         Mycam.m_XAxis.m_MaxSpeed = 300; 
         Mycam.m_YAxis.m_MaxSpeed = 2;
+        Debug.Log(b);
 
-        
 
-        
+
 
 
 
@@ -161,6 +188,22 @@ public class Ball_Controls : MonoBehaviour
 
         //question2
         if (other.CompareTag("RespawnTrigger2"))
+        {
+            ball.transform.position = RespawnPoint2.transform.position;
+            Physics.SyncTransforms();
+            myRB.velocity = Vector3.zero;
+        }
+
+        if (other.CompareTag("RepB2"))
+        {
+            ball.transform.position = RespawnPoint3.transform.position;
+            Physics.SyncTransforms();
+            myRB.velocity = Vector3.zero;
+            Question2.GetComponent<TextMeshProUGUI>().enabled = false;
+            Question3.GetComponent<TextMeshProUGUI>().enabled = true;
+        }
+
+        if (other.CompareTag("RepF2"))
         {
             ball.transform.position = RespawnPoint2.transform.position;
             Physics.SyncTransforms();
